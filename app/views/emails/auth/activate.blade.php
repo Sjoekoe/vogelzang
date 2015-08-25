172d13ada799df1de671fef623959d2f7e64d6f4
Sjoekoe <jonckershans@gmail.com>
2015-08-25 20:02:31 +0200

    adjust activation email

--- app/views/emails/auth/activate.blade.php
+++ app/views/emails/auth/activate.blade.php
@@ -2,6 +2,7 @@ Hey {{ $username }}
 <br><br>
 Gelieve uw account te activeren via onderstaande link. <br>
 Daarna kan u inloggen met de ontvangen gegevens.
+Wij raden u aan om zo snel mogelijk uw wachtwoord te wijzigen naar iets wat u kan onthouden.
 <br><br>
 Gebruikersnaam: {{ $username }} <br>
 Wachtwoord: {{ $password }}
