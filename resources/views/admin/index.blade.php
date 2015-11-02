420eb77f6488c38b7e7c091e9c7dda34c7656685
hans_J <hans@beatswitch.com>
2015-11-02 20:22:52 +0100

    Turn of error reporting

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,7 +8,7 @@
 		<h2>Welkom {{ Auth::user()->username }} </h2>
 		<div class="overview">
 			<p>Momenteel zijn er {{ \Vogelzang\User::all()->count() }} gebruikers waarvan {{ \Vogelzang\User::where('active', '=', 1)->count() }} geactiveerd.</p>
-			<p>Er zijn {{ Vogelzang\Models\Contact::all()->count() }} berichten waarvan {{ \Vogelzang\Models\Contact::where('read', '=', 0)->count() }} ongelezen.</p>
+			<p>Er zijn {{ \Vogelzang\Models\Contact::all()->count() }} berichten waarvan {{ \Vogelzang\Models\Contact::where('read', '=', 0)->count() }} ongelezen.</p>
 			<p>Er staan {{ \Vogelzang\Models\Horse::all()->count() }} paarden te koop</p>
 			<p>Er zijn {{ \Vogelzang\Models\Item::all()->count() }} nieuwsberichten gepubliceert</p>
 		</div>
